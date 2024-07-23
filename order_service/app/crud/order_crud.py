e6061db785e329814e3797538ab57befe5002ca9
Haroon Afzal <110262800+haroonafzal9090@users.noreply.github.com>
2024-07-24 02:25:03 +0500

    order-add-update

--- order_service/app/crud/order_crud.py
+++ order_service/app/crud/order_crud.py
@@ -4,17 +4,39 @@ from app.models.order_models import OrderItem
 
 
 # Add a New Order Item to the Database
+# def add_new_order_item(order_item_data: OrderItem, session: Session):
+#     try:
+#         existing_order = session.exec(select(OrderItem).where(OrderItem.id == order_item_data.id)).one_or_none()
+#         if existing_order:
+#             print(f"Error: An order with ID {order_item_data.id} already exists in the database.")
+#             return None
+        
+#         session.add(order_item_data)
+#         session.commit()
+#         session.refresh(order_item_data)
+#         return order_item_data
+#     finally:
+#         session.close()
+
+
 def add_new_order_item(order_item_data: OrderItem, session: Session):
     try:
-        existing_order = session.exec(select(OrderItem).where(OrderItem.id == order_item_data.id)).one_or_none()
-        if existing_order:
-            print(f"Error: An order with ID {order_item_data.id} already exists in the database.")
-            return None
-        
-        session.add(order_item_data)
-        session.commit()
-        session.refresh(order_item_data)
-        return order_item_data
+        existing_order_item = session.exec(select(OrderItem).filter(OrderItem.id == order_item_data.id)).one_or_none()
+        if existing_order_item:
+            # Item already exists, update the quantity
+            existing_order_item.quantity += order_item_data.quantity
+            session.commit()
+            session.refresh(existing_order_item)
+            return existing_order_item
+        else:
+            # Item does not exist, add new item
+            session.add(order_item_data)
+            session.commit()
+            session.refresh(order_item_data)
+            return order_item_data
+    except Exception as e:
+        session.rollback()
+        raise e
     finally:
         session.close()
 
